e51a3d510a9b0d424bf069e3910ecc6f3c73ede4
Adetoye Babalola <adetoyebamise@gmail.com>
2021-03-31 09:44:10 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// assign url path to a variable
+const baseUrl = 'https://www.balldontlie.io/api/v1/players'
+const contentDiv = document.querySelector('.content')
+
+// call fetch ; and response
+const getData = async () => {
+  const response = await fetch(`${baseUrl}`)
+  const data = await response.json()
+  return data
+}
+
+window.addEventListener('load', async () => {
+  const basketballPlayersData = await getData()
+  console.log(basketballPlayersData);
+  const secondBasketballPlayerData = basketballPlayersData[0]
+  
+  basketballPlayersData.forEach(basketball => 
+    contentDiv.innerHTML += `
+    <h2>${basketball.first_name}<h2>
+    <p>${basketball.last_name}</P>
+    `
+)
+})
